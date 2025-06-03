d232ad4c9ddc98dc9d2b6a066d6a0b874971da51
ymill016 <00083924@uca.edu.sv>
2025-06-03 17:28:15 -0600

    developing

--- commandcenter.cpp
+++ commandcenter.cpp
@@ -14,35 +14,14 @@ int numeroAleatorio(int min, int max) { //parametros para establecer un rango de
 //función de tipo booleana que regresa 1 ó 0 (verdadero o falso)
 
 //Área 1: Commmand Center
-bool commandCenter (char respuestaUno, char respuestaDos,char res){
-
-    switch (numeroAleatorio(1,3))
-    {
-    case 1:
-        cout << "Un tren eléctrico va de norte a sur. El viento sopla de este a oeste.\n¿Hacia dónde va el humo?"<< "\n";
-        cout <<"a. Hacia el sur\nb. Hacia el este\nc. Hacia ningún lado\n";
-        cout << "Respuesta(letra en minuscula): ";
-        cin >> respuestaUno;
-
-        if (respuestaUno == 'c'){
-            cout << "¡Tarea completada!\nIngresa 1 para regresar.\n";
-
-        }
-        break;
-    case 2:
-    break;
-
-    case 3:
-    break;
-    
-    default:
-        break;
-    }
-
-    return 0;
+bool commandCenter (string pregunta, char respuestaCorrecta){
+    char respuestaIngresada;
+    cout << pregunta << "\n";
+    cin >> respuestaIngresada;
+    return respuestaIngresada == respuestaCorrecta;
 }
 
+
 int main (){
-    srand(time(0));
     return 0;
 }
