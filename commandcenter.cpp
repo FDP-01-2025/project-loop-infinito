ed6079eec3344824f964232ecc743b7554a5b266
ymill016 <00083924@uca.edu.sv>
2025-06-03 16:46:34 -0600

    developing

--- commandcenter.cpp
+++ commandcenter.cpp
@@ -2,14 +2,18 @@
 tarea de resolver un acertijo simple*/
 
 #include <iostream>
-#include <ctime>
+#include <ctime> 
 using namespace std;
 
-
-int numeroAleatorio(int min, int max) {
+//función de numeros aleatorios
+int numeroAleatorio(int min, int max) { //parametros para establecer un rango de numero aleatorio
     return min + rand() % (max - min + 1);
 }
 
+
+//función de tipo booleana que regresa 1 ó 0 (verdadero o falso)
+
+//Área 1: Commmand Center
 bool commandCenter (char respuestaUno, char respuestaDos,char res){
 
     switch (numeroAleatorio(1,3))
@@ -17,7 +21,13 @@ bool commandCenter (char respuestaUno, char respuestaDos,char res){
     case 1:
         cout << "Un tren eléctrico va de norte a sur. El viento sopla de este a oeste.\n¿Hacia dónde va el humo?"<< "\n";
         cout <<"a. Hacia el sur\nb. Hacia el este\nc. Hacia ningún lado\n";
-        cin <<
+        cout << "Respuesta(letra en minuscula): ";
+        cin >> respuestaUno;
+
+        if (respuestaUno == 'c'){
+            cout << "¡Tarea completada!\nIngresa 1 para regresar.\n";
+
+        }
         break;
     case 2:
     break;
