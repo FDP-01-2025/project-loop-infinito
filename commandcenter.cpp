769aa74bf6a73e0054f7fc81927f78328164f6ea
ymill016 <00083924@uca.edu.sv>
2025-06-03 16:28:36 -0600

    developing

--- commandcenter.cpp
+++ commandcenter.cpp
@@ -1,1 +1,38 @@
+/*Area número 1: Command Center
+tarea de resolver un acertijo simple*/
 
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+
+int numeroAleatorio(int min, int max) {
+    return min + rand() % (max - min + 1);
+}
+
+bool commandCenter (char respuestaUno, char respuestaDos,char res){
+
+    switch (numeroAleatorio(1,3))
+    {
+    case 1:
+        cout << "Un tren eléctrico va de norte a sur. El viento sopla de este a oeste.\n¿Hacia dónde va el humo?"<< "\n";
+        cout <<"a. Hacia el sur\nb. Hacia el este\nc. Hacia ningún lado\n";
+        cin <<
+        break;
+    case 2:
+    break;
+
+    case 3:
+    break;
+    
+    default:
+        break;
+    }
+
+    return 0;
+}
+
+int main (){
+    srand(time(0));
+    return 0;
+}
