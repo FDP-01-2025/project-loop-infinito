d82853eabefc2f6bf8af799296d870bab32bd36d
ymill016 <00083924@uca.edu.sv>
2025-06-03 18:14:43 -0600

    developing

--- commandcenter.cpp
+++ commandcenter.cpp
@@ -2,41 +2,48 @@
 tarea de resolver un acertijo simple*/
 
 #include <iostream>
-#include <ctime> 
+#include <ctime>
 using namespace std;
 
-//función de numeros aleatorios
-int numeroAleatorio(int min, int max) { //parametros para establecer un rango de numero aleatorio
+// función de numeros aleatorios
+int numeroAleatorio(int min, int max)
+{ // parametros para establecer un rango de numero aleatorio
     return min + rand() % (max - min + 1);
 }
 
+// función de tipo booleana que regresa 1 ó 0 (verdadero o falso)
 
-//función de tipo booleana que regresa 1 ó 0 (verdadero o falso)
-
-//Área 1: Commmand Center
-bool flujoCommandCenter (string pregunta, char respuestaCorrecta){
+// Área 1: Commmand Center
+//funcion para cambiar de acertijos y su respuestas
+bool flujoCommandCenter(string pregunta, char respuestaCorrecta)
+{
     char respuestaIngresada;
     cout << pregunta << "\n";
     cin >> respuestaIngresada;
     return respuestaIngresada == respuestaCorrecta;
 }
-
-int commandCenter (){
-    int aleatorio = numeroAleatorio (1,3);
+//funcion principal de command center
+bool commandCenter()
+{
+    int aleatorio = numeroAleatorio(1, 3);
 
     switch (aleatorio)
     {
     case 1:
-        cout << flujoCommandCenter("Un tren eléctrico va de norte a sur. El viento sopla de este a oeste.\n¿Hacia dónde va el humo?\na. Hacia el Norte\nb. Hacia el oeste\nc. Hacia ningún lado\n",'c');
+        return flujoCommandCenter("Un tren eléctrico va de norte a sur. El viento sopla de este a oeste.\n¿Hacia dónde va el humo?\na. Hacia el Norte  b. Hacia el oeste  c. Hacia ningún lado\n", 'c');
         break;
     case 2:
-    cout << flujoCommandCenter()
-    default:
+        return flujoCommandCenter("¿Qué palabra de cuatro letras contiene seis?\na. ocho  b. seis  c. luna\n", 'b');
         break;
-    }
-    }
+    case 3:
+        return flujoCommandCenter("¿Qué sube, pero nunca baja?\na. Tu edad  b. El sol  c. La marea\n", 'a');
 
+    default:
+        return;
+    }
+}
 
-int main (){
+int main()
+{
     return 0;
 }
