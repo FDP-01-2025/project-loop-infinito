4bdac193b12b9c5bd21ff8f16899f152092ef0cd
ymill016 <00083924@uca.edu.sv>
2025-06-03 22:04:26 -0600

    developing

--- crewQuarters.cpp
+++ crewQuarters.cpp
@@ -4,11 +4,25 @@
 #include <ctime>
 using namespace std;
 
+// función de numeros aleatorios
+int numeroAleatorio(int min, int max)
+{ // parametros para establecer un rango de numero aleatorio
+    return min + rand() % (max - min + 1);
+}
 
 bool crewQuarters(){
-
+    int contra = numeroAleatorio(1000,9999);
+    int regresar;
+    cout << "Memoriza esta constraseña/codigo: "<< contra << "\nEs una Mouseketool que nos ayudará mas tarde\n";
+    cout << "Ingresa 1 para completar la tarea: ";
+    cin >> regresar;
+    return true;
 }
 
 int main (){
+    if (crewQuarters() == 1){
+        cout << "Tarea completada";
+    }
 
+    return 0;
 }
